2751bcf09b7a46e6415da365714ddeaa8515a877
Irakli <112602982+IkaMastera@users.noreply.github.com>
2024-06-24 15:41:49 +0400

    refactor: product list card layout

--- scandiweb-frontend/src/components/ProductCard/ProductCard.js
+++ scandiweb-frontend/src/components/ProductCard/ProductCard.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./ProductCard.css";
+import emptyCart from "../../images/emptycart.svg";
 
 const ProductCard = ({ product, addToCart }) => {
   let gallery = [];
 
-  // Try to parse the gallery JSON string into an array
   try {
     gallery = JSON.parse(product.gallery);
   } catch (error) {
@@ -18,19 +18,15 @@ const ProductCard = ({ product, addToCart }) => {
 
   return (
     <div className="product-card">
-      <img
-        src={gallery[0]} // Display the first image
-        alt={product.name}
-        className="product-image"
-      />
-      <h2>{product.name}</h2>
-      <p>
+      <img src={gallery[0]} alt={product.name} className="product-image" />
+      <h2 className="product-name">{product.name}</h2>
+      <p className="product-price">
         {product.prices[0].currency_symbol}
         {product.prices[0].amount}
       </p>
       {product.in_stock ? (
         <button onClick={() => addToCart(product)} className="add-to-cart">
-          <img src="/path/to/cart-icon.png" alt="Add to cart" />
+          <img src={emptyCart} alt="Add to cart" />
         </button>
       ) : (
         <div className="out-of-stock">Out Of Stock</div>
